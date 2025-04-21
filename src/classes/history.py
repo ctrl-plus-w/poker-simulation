200d64d1729e2ba31c5a04c984f5d55770424aa4
Lukas Laudrain <lukas.ldrn@gmail.com>
2025-04-22 00:28:04 +0200

    feat: increase now by 1 minute for each history entry

--- src/classes/history.py
+++ src/classes/history.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from typing import Optional
 
 from src.classes.sql_statement_builder import SQLInsertStatementBuilder
@@ -7,18 +7,28 @@ from src.enums.action import Action
 
 current_id = 1
 
+now = datetime.now(timezone.utc)
+
+
+def get_now():
+    global now
+    now += timedelta(0, 0, 0, 0, 1)
+    print(now)
+    return now
+
 
 class History:
     id: int = None
     action: Action = None
     value: Optional[int] = None
     round_num: int = None
-    created_at: datetime = datetime.now(timezone.utc)
+    created_at: datetime = None
 
     def __init__(self, action: Action, value: Optional[int], round_num: int):
         self.action = action
         self.value = value
         self.round_num = round_num
+        self.created_at = get_now()
 
         global current_id
         self.id = current_id
